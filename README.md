e2bd2be08014810c2ebfb21fee674aea64023bcf
Yusuf Gül <yuusuufguul@gmail.com>
2023-02-22 19:03:50 +0300

    formatting

---
+++ README.md
@@ -0,0 +1,17 @@
+# Java_Exercise
+
+When this program is executed, the user is asked to select one of the six sub-programs or the exit process. The menu looks like the following:<br />
+
+...................................................................................................<br />
+...................................................................................................<br />
+Please select a program from below to continue.<br />
+...................................................................................................<br />
+...................................................................................................<br />
+
+1- Array operations.<br />
+2- Number guessing game. <br />
+3- Find repeating numbers in a array.<br />
+4- Find the sum of the digits of a number. <br />
+5- Draw an equilateral triangle from numbers. <br />
+6- Move symbol inside star array.<br />
+7- Exit.<br />
--- src/yusuf/com/drawEquilateralTriangle.java
+++ src/yusuf/com/drawEquilateralTriangle.java
@@ -3,46 +3,42 @@ package yusuf.com;
 import java.util.Scanner;
 
 public class drawEquilateralTriangle {
-	
-public static void drawTriangle() {
-	
-	 boolean end = false;
-	
-	while(!end) {
-		
-         int i=1,number;
-         Scanner sc = new Scanner(System.in);
-         System.out.println("Enter a number: ");
-         number = sc.nextInt();
 
-   for(int k=1; k<=number; k++) {
-        for(int j=1;j<=number-i;j++)
-        {
+  public static void drawTriangle() {
+    boolean end = false;
+
+    while (!end) {
+      int i = 1, number;
+      Scanner sc = new Scanner(System.in);
+      System.out.println("Enter a number: ");
+      number = sc.nextInt();
+
+      for (int k = 1; k <= number; k++) {
+        for (int j = 1; j <= number - i; j++) {
           System.out.print(" ");
         }
-        for(i=1; i<=k; i++) { 
-          System.out.print(k+" ");
+        for (i = 1; i <= k; i++) {
+          System.out.print(k + " ");
         }
-          System.out.println("");
-       }//end of outer for method
-     end = repeat();
-    }//end of while method
-}//end of drawTriangle method
+        System.out.println("");
+      } //end of outer for method
+      end = repeat();
+    } //end of while method
+  } //end of drawTriangle method
 
-public static boolean repeat() {
-	
-	Scanner sc = new Scanner(System.in);
-	char playAgain = 'n';
-	boolean end = false;
-	
-	  System.out.println("\nDo you want to draw another triangle?(y/n): ");
-	  playAgain = sc.next().charAt(0);
-	  
-	  if(playAgain=='n') {//user wants to quit
-	      System.out.println("\nThe program has been terminated successfully.");
-	      System.out.println("\nNow returning to the main menu...");
-	      end = true;
-	  }//end of if loop
-	  return end;
-   }//end of repeat method
-}//end of drawEquilateralTriangle class
+  public static boolean repeat() {
+    Scanner sc = new Scanner(System.in);
+    char playAgain = 'n';
+    boolean end = false;
+
+    System.out.println("\nDo you want to draw another triangle?(y/n): ");
+    playAgain = sc.next().charAt(0);
+
+    if (playAgain == 'n') { //user wants to quit
+      System.out.println("\nThe program has been terminated successfully.");
+      System.out.println("\nNow returning to the main menu...");
+      end = true;
+    } //end of if loop
+    return end;
+  } //end of repeat method
+} //end of drawEquilateralTriangle class
